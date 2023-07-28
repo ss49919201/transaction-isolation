35ddfa32c0a03cfa5053bd9bea072e93a4552452
ss49919201 <sakashin10291029@gmail.com>
2023-07-28 22:40:42 +0900

    refactor

--- example/dirty_read.go
+++ example/dirty_read.go
@@ -10,53 +10,64 @@ import (
 
 // dirty read
 // 他のTXの**未コミット**の更新が参照できてしまう
+// 更新 => INSERT,UPDATE,DELETE
 func main() {
 	// DBではなくConnを生成する必要あり
-	db, err := sql.Open("mysql", dsn())
-	if err != nil {
-		panic(err)
-	}
-
-	conn, err := db.Conn(context.Background())
-	if err != nil {
-		panic(err)
-	}
-
-	conn2, err := db.Conn(context.Background())
-	if err != nil {
-		panic(err)
-	}
+	db := openDB()
+	conn := newConn(db)
+	conn2 := newConn(db)
 
 	// dirty read を発生させたいセッションのみ READ UNCOMMITTED にしておく
-	_, err = conn.QueryContext(context.Background(), "SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED")
-	if err != nil {
-		panic(err)
-	}
+	setTXLevel(conn)
 
-	tx, err := conn.BeginTx(context.Background(), nil)
-	if err != nil {
-		panic(err)
-	}
+	tx := newTx(conn)
+	tx2 := newTx(conn2)
 
-	tx2, err := conn2.BeginTx(context.Background(), nil)
-	if err != nil {
-		panic(err)
-	}
-	_, err = tx2.Query("UPDATE tbl SET counter = 2 WHERE id = 1")
-	if err != nil {
+	// counterを1から2に更新
+	if _, err := tx2.Query("UPDATE tbl SET counter = 2 WHERE id = 1"); err != nil {
 		panic(err)
 	}
 
 	// dirty read
 	var counter int
-	err = tx.QueryRow("SELECT counter FROM tbl WHERE id = 1").Scan(&counter)
-	if err != nil {
+	if err := tx.QueryRow("SELECT counter FROM tbl WHERE id = 1").Scan(&counter); err != nil {
 		panic(err)
 	}
 	fmt.Println("tx counter: ", counter) // 別のTXの未コミットの更新が参照できるので 2 が出力される
 
 	tx.Rollback()
 	tx2.Rollback()
+}
+
+func openDB() *sql.DB {
+	db, err := sql.Open("mysql", dsn())
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func newConn(db *sql.DB) *sql.Conn {
+	conn, err := db.Conn(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	return conn
+}
+
+func setTXLevel(conn *sql.Conn) {
+	_, err := conn.QueryContext(context.Background(), "SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func newTx(conn *sql.Conn) *sql.Tx {
+	tx, err := conn.BeginTx(context.Background(), nil)
+	if err != nil {
+		panic(err)
+	}
+	return tx
 }
 
 func dsn() string {
